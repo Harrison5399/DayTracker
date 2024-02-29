0d1a75748e12f443a16b96301fbad880930d011b
Harrison <Harrison353674@gmail.com>
2024-02-29 14:52:20 -0500

    Initial Commit and Push

--- src/routes/DayActivity/editChartProperties.js
+++ src/routes/DayActivity/editChartProperties.js
@@ -6,12 +6,46 @@ const router=express.Router()
 
 router.post("/",(req,res,next)=>{
 
+    /*
+    req JSON:
+    {
+        "global": {
+            "property": "value",
+            "property": "value",
+            "property": "value"
+        }
+    }
+
+    OR, sort by only key (global or local) OR what if can have both and see if keys contains either
+
+    {
+        "local": {
+            "label": {
+                "property" : "value",
+                "property" : "value",
+                "property" : "value"
+            }
+        }
+    }
+     */
+
     let req_data = req.body
-    console.log('req_data:')
-    console.log(req_data)
     let data = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data', activitieschartproperties  + '.json'), "utf-8"))
+
+    if (Object.keys(req_data).includes("global")){
+        data.global = {
+            ...req_data.global
+        }
+    }
+    if (Object.keys(req_data).includes("local")){
+        for (let i in req_data.local) {
+            data.local[i] = {
+                ...req_data.local[i]
+            }
+        }
+    }
+
     res.setHeader('Content-Type', 'application/json')
-    console.log("\n\n")
     res.json(data)
     res.end()
 
