0fde6c3e2aae71b215150a3a5eac7fc89e4e9147
Harrison <Harrison353674@gmail.com>
2024-02-29 14:43:07 -0500

    Initial Commit and Push

--- src/routes/DayActivity/editChartProperties.js
+++ src/routes/DayActivity/editChartProperties.js
@@ -9,7 +9,7 @@ router.post("/",(req,res,next)=>{
     let req_data = req.body
     console.log('req_data:')
     console.log(req_data)
-    let data = JSON.parse(fs.readFileSync('src/data/activitieschartproperties.json', "utf-8"))
+    let data = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data', activitieschartproperties  + '.json'), "utf-8"))
     res.setHeader('Content-Type', 'application/json')
     console.log("\n\n")
     res.json(data)
