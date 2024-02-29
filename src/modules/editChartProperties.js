113be0811a8e7d05057a30e14cec60390f4b557f
Harrison <Harrison353674@gmail.com>
2024-02-29 17:29:30 -0500

    Initial Commit and Push

--- src/modules/editChartProperties.js
+++ src/modules/editChartProperties.js
@@ -3,8 +3,8 @@ fetch('/DayActivity/getChartProperties/')
     .then((chart_properties) => {
         console.log(chart_properties)
 
-        const global_title = document.createElement('h4')
-        global_title.innerText = '\tglobal:'
+        const global_title = document.createElement('h3')
+        global_title.innerText = '&nbsp; global:'
         document.body.appendChild(global_title)
 
         const global_container = document.createElement('div')
@@ -12,7 +12,7 @@ fetch('/DayActivity/getChartProperties/')
 
         for (let global_k in chart_properties.global) {
             const container = document.createElement('div')
-            container.className = 'keyvalue'
+            container.className = 'key_value'
 
             const key = document.createElement('p')
             key.innerText = global_k + ' : '
@@ -29,4 +29,39 @@ fetch('/DayActivity/getChartProperties/')
         }
 
         document.body.appendChild(global_container)
+
+        const local_title = document.createElement('h3')
+        local_title.innerText = '&nbsp; local:'
+        document.body.appendChild(local_title)
+
+        const local_container = document.createElement('div')
+        local_container.id = 'local_container'
+
+        for (let local_k in chart_properties.local) {
+
+            const label_properties = document.createElement('div')
+
+            const label = document.createElement('h5')
+            label.innerText = local_k
+            label_properties.appendChild(label)
+
+            for (let property in chart_properties.local[local_k]) {
+                const container = document.createElement('div')
+                container.className = 'key_value'
+
+                const key = document.createElement('p')
+                key.innerText = property + ' : '
+                container.appendChild(key)
+
+                const value = document.createElement('input')
+                value.type = 'text'
+                value.className = 'local'
+                value.name = property
+                value.placeholder = chart_properties.local[property]
+                container.appendChild(value)
+
+                label_properties.appendChild(container)
+            }
+            document.body.appendChild(label_properties)
+        }
     })
