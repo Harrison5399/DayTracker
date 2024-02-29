126cd6435e0e05801ca6741b0cd19310b07d1d71
Harrison <Harrison353674@gmail.com>
2024-02-29 17:20:28 -0500

    Initial Commit and Push

--- src/modules/editChartProperties.js
+++ src/modules/editChartProperties.js
@@ -7,6 +7,9 @@ fetch('/DayActivity/getChartProperties/')
         global_title.innerText = '\tglobal:'
         document.body.appendChild(global_title)
 
+        const global_container = document.createElement('div')
+        global_container.id = 'global_container'
+
         for (let global_k in chart_properties.global) {
             const container = document.createElement('div')
             container.className = 'keyvalue'
@@ -22,6 +25,8 @@ fetch('/DayActivity/getChartProperties/')
             value.placeholder = chart_properties.global[global_k]
 
             container.appendChild(value)
-            document.body.appendChild(container)
+            global_container.appendChild(container)
         }
+
+        document.body.appendChild(global_container)
     })
