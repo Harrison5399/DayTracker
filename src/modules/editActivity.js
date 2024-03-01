f0a1ecf41473c1ef12f1501dfa5afdf17f33b628
Harrison <Harrison353674@gmail.com>
2024-02-29 23:31:56 -0500

    Initial Commit and Push

--- src/modules/editActivity.js
+++ src/modules/editActivity.js
@@ -1,20 +1,23 @@
-
 // initialize form
+let container = document.createElement('div')
+container.className = 'container'
+document.body.appendChild(container)
+
 let input_title = document.createElement('h2')
 input_title.innerText = 'label:'
-document.appendChild(input_title)
+container.appendChild(input_title)
 
 let input = document.createElement('input')
 input.type = 'text'
 input.name = 'label'
 input.id = 'label'
-document.appendChild(input)
+container.appendChild(input)
 
 let  btn = document.createElement('input')
 btn.type = 'button'
 btn.value = 'submit'
 btn.onclick = function(){postEdit()};
-document.appendChild(btn)
+container.appendChild(btn)
 
 function postEdit() {
     let event = document.getElementById('label').value
